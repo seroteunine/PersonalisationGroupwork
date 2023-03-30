1852b4c4d1e9ee8afeffdc2261ada234640913bf
seroteunine <teun.vandalen.tvd@gmail.com>
2023-03-30 17:19:30 +0200

    intermediate commit: added age demographic

--- app/generator.py
+++ app/generator.py
@@ -1,15 +1,14 @@
 # Setup
 import random
 import names
-from tqdm import tqdm
 # Custom modules
 from data_import import data_import, json_dump
 
 # Import data
 df, df_activity, df_users, word_scores = data_import(overwrite_text=False, overwrite_activity=False) 
 
-N_PERSONAS = int(3) #1 = left, 2 = neutral, 3 = right
-N_USERS_PER_PERSONA = int(3e3)
+#N_PERSONAS = int(3) #1 = left, 2 = neutral, 3 = right
+N_USERS = int(100)
 
 users = []
 activities = []
@@ -24,6 +23,13 @@ combined = [left_shows, neutral_shows, right_shows]
 # Generate a single activity based on pre-defined logic/personas
 def generate_activity(id, persona, activity):
     #Get show
+    if persona == 'left':
+       persona = 1
+    elif persona == 'right':
+       persona = 3
+    else:
+       persona = 2
+
     rand = random.random()
     if rand < 0.1: #Choose random show to watch
         show = random.sample(show_titles.tolist(), 1)
@@ -54,17 +60,28 @@ def generate_activity(id, persona, activity):
 
     return activity
 
+def getVote(age):
+   return 'left'
+
 # Generate users and activities
-for persona in range(N_PERSONAS):
-  for user in tqdm(range(N_USERS_PER_PERSONA)):
+for user in range(N_USERS):
     #Create userdata
-    id = user + persona * N_USERS_PER_PERSONA + 1
-    data = {'name': names.get_first_name() + str(id), 'id': id, 'password': 'recommender'}
+    id = user + 1
+    if id <= 44: #Young generation 16-34
+       age = random.sample(range(20,35), 1)[0]
+    elif id < 84: #Middle generation 35-54
+       age = random.sample(range(35, 55), 1)[0]
+    else: #Old generation 55+
+       age = random.sample(range(55, 71), 1)[0]
+
+    vote = getVote(age)
+
+    data = {'name': names.get_first_name() + str(id), 'id': id, 'password': 'recommender', 'age': age, 'vote': vote}
 
     #Create user activity
     activity = []
     for n in range(random.randint(10, 50)):
-      new_activity = generate_activity(id, persona, activity)
+      new_activity = generate_activity(id, vote, activity)
     activities.extend(activity)
     users.append(data)
 
